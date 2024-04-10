1cf8ba409ea9c0886d1a4fca3a0befa5c862ee5e
Asmerom Estifanos <asmerom.estifanose@gmail.com>
2024-04-10 21:03:36 +0300

    Setup cloud sync with Atlas App Service

--- src/providers/Realm.tsx
+++ src/providers/Realm.tsx
@@ -10,7 +10,20 @@ const RealmCustomProvider = ({ children }: PropsWithChildren) => {
   return (
     <AppProvider id={appId}>
       <UserProvider fallback={Login}>
-        <RealmProvider schema={[Task]}>{children}</RealmProvider>
+        <RealmProvider
+          schema={[Task]}
+          sync={{
+            flexible: true,
+            initialSubscriptions: {
+              update(subs, realm) {
+                subs.add(realm.objects("Task"));
+              },
+              rerunOnOpen: true,
+            },
+          }}
+        >
+          {children}
+        </RealmProvider>
       </UserProvider>
     </AppProvider>
   );
